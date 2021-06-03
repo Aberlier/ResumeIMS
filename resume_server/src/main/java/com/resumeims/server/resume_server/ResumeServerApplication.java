0923b05ad99ea058f38fd7ddc0a3ca58201eb8b7
ChiLi-zy <1162995411@qq.com>
2021-06-04 00:09:19 +0800

    初始化项目

---
+++ resume_server/src/main/java/com/resumeims/server/resume_server/ResumeServerApplication.java
@@ -0,0 +1,13 @@
+package com.resumeims.server.resume_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResumeServerApplication.class, args);
+    }
+
+}
---
+++ resume_server/src/test/java/com/resumeims/server/resume_server/ResumeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resumeims.server.resume_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
